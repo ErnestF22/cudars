43cd31b323c7aef22511a38c68b9fbddf8cdbd6a
ErnestF22 <ernesto.fontana96@gmail.com>
2022-05-17 17:02:19 +0200

    Added plot grid option; improved formatting

--- test/gpu_testConsensusTranslationEstimator.cu
+++ test/gpu_testConsensusTranslationEstimator.cu
@@ -4,8 +4,7 @@
 #include <ars/ConsensusTranslationEstimator.cuh>
 #include <rofl/common/param_map.h>
 
-
-// void plotGrid(const cuars::ConsensusTranslationEstimator2d::Grid& grid, const cuars::Vec2d& translMin, double translRes, const std::string& filename, double factor);
+void plotGrid(const cuars::ConsensusTranslationEstimator2d::Grid &grid, const cuars::Vec2d &translMin, double translRes, const std::string &filename, double factor);
 
 int main(int argc, char **argv)
 {
@@ -16,6 +15,7 @@ int main(int argc, char **argv)
     typename cuars::ConsensusTranslationEstimator2d::Indices gridSize, gridWin;
     rofl::ParamMap params;
     std::string filenameCfg;
+    bool plot;
 
     // Reads params from command line
     params.read(argc, argv);
@@ -25,12 +25,13 @@ int main(int argc, char **argv)
     params.getParam<double>("translRes", translRes, 1.0);
     // params.getParamContainer("translMin", translMin.data(), translMin.data() + translMin.size(), "[-10.0,-10.0]", double(0.0), "[,]"); //TODO: adapt ParamContainer to Cuda types
     params.getParam<double>("translMin-x", translMin.x, -10.0);
-    params.getParam<double>("translMin-y", translMin.y, -10.0);  
+    params.getParam<double>("translMin-y", translMin.y, -10.0);
     // params.getParamContainer("translGt", translGt.data(), translGt.data() + translGt.size(), "[-4.2,5.0]", double(1.0), "[,]");
     params.getParam<double>("translGt-x", translGt.x, -4.2);
-    params.getParam<double>("translGt-y", translGt.y, 5.0);  
+    params.getParam<double>("translGt-y", translGt.y, 5.0);
     params.getParamContainer("gridSize", gridSize.data(), gridSize.data() + gridSize.size(), "[21,21]", int(0), "[,]");
     params.getParamContainer("gridWin", gridWin.data(), gridWin.data() + gridWin.size(), "[1,1]", int(1), "[,]");
+    params.getParam<bool>("plot", plot, false);
 
     std::cout << "\nParams:" << std::endl;
     params.write(std::cout);
@@ -52,14 +53,16 @@ int main(int argc, char **argv)
     for (auto &pt : pointsSrc)
     {
         std::cout << "  [";
-        cuars::printVec2d(pt);
+        // cuars::printVec2d(pt);
+        std::cout << pt.x << "\t" << pt.y;
         std::cout << "]\n";
     }
     std::cout << "Destination point set:\n";
     for (auto &pt : pointsDst)
     {
         std::cout << "  [";
-        cuars::printVec2d(pt);
+        // cuars::printVec2d(pt);
+        std::cout << pt.x << "\t" << pt.y;
         std::cout << "]\n";
     }
 
@@ -69,7 +72,8 @@ int main(int argc, char **argv)
     std::cout << "Inserting pair source-destination:\n";
     translEstim.insert(pointsSrc, pointsDst);
 
-    // plotGrid(translEstim.getGrid(), translMin, translRes, "consensus_transl_grid.plot", 1.0);
+    if (plot)
+        plotGrid(translEstim.getGrid(), translMin, translRes, "consensus_transl_grid.plot", 1.0);
 
     std::cout << "Computing maxima:\n";
     translEstim.computeMaxima(translCandidates);
@@ -78,39 +82,50 @@ int main(int argc, char **argv)
     for (auto &pt : translCandidates)
     {
         std::cout << "  [";
-        cuars::printVec2d(pt);
+        // cuars::printVec2d(pt);
+        std::cout << pt.x << "\t" << pt.y;
         std::cout << "]\n";
     }
 
     return 0;
 }
 
-// void plotGrid(const cuars::ConsensusTranslationEstimator2d::Grid& grid, const cuars::Vec2d& translMin, double translRes, const std::string& filename, double factor) {
-//     int dim0 = grid.dimensions()[0];
-//     int dim1 = grid.dimensions()[1];
-//     int dim0Reduced = round(dim0 / factor);
-//     int dim1Reduced = round(dim1 / factor);
-//     double v0, v1;
+void plotGrid(const cuars::ConsensusTranslationEstimator2d::Grid &grid, const cuars::Vec2d &translMin, double translRes, const std::string &filename, double factor)
+{
+    int dim0 = grid.dimensions()[0];
+    int dim1 = grid.dimensions()[1];
+    int dim0Reduced = round(dim0 / factor);
+    int dim1Reduced = round(dim1 / factor);
+    double v0, v1;
 
-//     std::ofstream file(filename);
-//     if (!file) {
-//         std::cerr << "Cannot open \"" << filename << "\"" << std::endl;
-//         return;
-//     }
+    std::ofstream file(filename);
+    if (!file)
+    {
+        std::cerr << "Cannot open \"" << filename << "\"" << std::endl;
+        return;
+    }
 
-//     std::cout << "plotting grid with size " << dim0 << " x " << dim1 << std::endl;
+    std::cout << "plotting grid with size " << dim0 << " x " << dim1 << std::endl;
 
-//     file << "set grid nopolar\n" << "set style data lines\n" << "set dgrid3d " << dim0Reduced << "," << dim1Reduced << "\n" << "set hidden3d\n";
+    file << "set grid nopolar\n"
+         << "set style data lines\n"
+         << "set dgrid3d " << dim0Reduced << "," << dim1Reduced << "\n"
+         << "set hidden3d\n";
 
-//     file << "splot '-'\n";
-//     for (int i0 = 0; i0 < dim0; ++i0) {
-//         for (int i1 = 0; i1 < dim1; ++i1) {
-//             v0 = translMin(0) + translRes * i0;
-//             v1 = translMin(1) + translRes * i1;
-//             file << v0 << " " << v1 << " " << grid.value({i0, i1}) << "\n";
-//         }
-//     }
-//     file << "e\n";
+    file << "splot '-'\n";
+    for (int i0 = 0; i0 < dim0; ++i0)
+    {
+        for (int i1 = 0; i1 < dim1; ++i1)
+        {
+            // v0 = translMin(0) + translRes * i0;
+            v0 = translMin.x + translRes * i0;
+            // v1 = translMin(1) + translRes * i1;
+            v1 = translMin.y + translRes * i1;
 
-//     file.close();
-// }
+            file << v0 << " " << v1 << " " << grid.value({i0, i1}) << "\n";
+        }
+    }
+    file << "e\n";
+
+    file.close();
+}
