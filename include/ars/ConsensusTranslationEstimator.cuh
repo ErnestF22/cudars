dee2fb1bc1a960fc30c1dd61d5928a448d497da6
ErnestF22 <ernesto.fontana96@gmail.com>
2022-05-18 16:04:26 +0200

    Re-separated functions called from computeMaxima()

--- include/ars/ConsensusTranslationEstimator.cuh
+++ include/ars/ConsensusTranslationEstimator.cuh
@@ -30,68 +30,64 @@ namespace cuars
     using Point = typename MakePt<Scalar, 2>::type; // for now we work with 2d
     using VectorPoint = thrust::host_vector<Point>;
 
-    // template <typename Grid, typename Indices, typename PeakFinder, size_t Dim, typename Scalar = double>
-    // void computeMaximaInd(std::vector<Indices> &indicesMax, Grid &grid_, PeakFinder &peakFinder_)
-    // {
-    //     auto histoMap = [&](const Indices &indices) -> Counter
-    //     {
-    //         // ARS_VARIABLE3(indices[0], indices[1], grid_.inside(indices));
-    //         return grid_.value(indices);
-    //     };
-    //     peakFinder_.detect(histoMap, std::back_inserter(indicesMax));
+    // Point getTranslation(const Indices &indices) const;
+    template <typename Indices, size_t Dim, typename Scalar = double>
+    Point getTranslation(const Indices &indices, Point &translMin_, Scalar &translRes_)
+    {
+        Point transl;
 
-    //     //            ARS_PRINT("Maxima:");
-    //     //            for (auto &idx : indicesMax) {
-    //     //                std::cout << "  indices [" << idx[0] << "," << idx[1] << "] value " << histoMap(idx)
-    //     //                        << " grid2.value() " << grid_.value(idx) << std::endl;
-    //     //            }
-    // } // TODO: declare the real function
+        for (int d = 0; d < Dim; ++d)
+        {
+            // transl(d) = translRes_ * indices[d] + translMin_(d);
+            idxSetter(transl, d, translRes_ * indices[d] + idxGetter(translMin_, d));
+        }
+
+        return transl;
+    }
+
+    template <typename Grid, typename Indices, typename PeakFinder, size_t Dim, typename Scalar = double>
+    void computeMaximaInd(std::vector<Indices> &indicesMax, Grid &grid_, PeakFinder &peakFinder_)
+    {
+        auto histoMap = [&](const Indices &indices) -> Counter
+        {
+            // ARS_VARIABLE3(indices[0], indices[1], grid_.inside(indices));
+            return grid_.value(indices);
+        };
+        peakFinder_.detect(histoMap, std::back_inserter(indicesMax));
+
+        //            ARS_PRINT("Maxima:");
+        //            for (auto &idx : indicesMax) {
+        //                std::cout << "  indices [" << idx[0] << "," << idx[1] << "] value " << histoMap(idx)
+        //                        << " grid2.value() " << grid_.value(idx) << std::endl;
+        //            }
+    } // TODO: declare the real function
 
     template <typename Grid, typename Indices, typename PeakFinder, size_t Dim, typename Scalar = double>
     void computeMaxima(VectorPoint &translMax, Grid &grid, PeakFinder &peakFinder, Point &translMin_, Scalar &translRes_)
     {
+
         std::vector<Indices> indicesMax;
-        // computeMaximaInd(indicesMax, grid, peakFinder); //expanding this function below
-        auto histoMap = [&](const Indices &indices) -> Counter
-        {
-            // ARS_VARIABLE3(indices[0], indices[1], grid_.inside(indices));
-            return grid.value(indices);
-        };
-        peakFinder.detect(histoMap, std::back_inserter(indicesMax));
-        //end of expanded function
+        computeMaximaInd<Grid, Indices, PeakFinder, Dim>(indicesMax, grid, peakFinder); 
 
         translMax.clear();
         translMax.reserve(indicesMax.size());
-        for (auto idx : indicesMax)
+        // for (auto idx : indicesMax) //cannot really use foreach here...
+        for (Counter i=0; i<indicesMax.size(); ++i)
         {
-            // Point p = getTranslation(idx); //expanding this function below
-            Point p;
-            for (int d = 0; d < Dim; ++d)
-            {
-                // transl(d) = translRes_ * indices[d] + translMin_(d);
-                idxSetter(p, d, translRes_ * idx[d] + idxGetter(translMin_, d));
-            }
-            //end of expanded function
+            Indices idx = indicesMax.at(i);
+
+            Point p = getTranslation<Indices, Dim>(idx, translMin_, translRes_); // expanding this function below
 
             //                ARS_VAR4(idx[0], idx[1], grid_.value(idx), p.transpose());
             translMax.push_back(p);
         }
     }
 
-    // Point getTranslation(const Indices &indices) const
-    // template <typename Indices, size_t Dim, typename Scalar = double>
-    // Point getTranslation(const Indices &indices, Point &translMin_, Scalar &translRes_)
-    // {
-    //     Point transl;
-
-    //     return transl;
-    // } // TODO: declare the real function
-
+    template Point getTranslation<Indices2d, 2>(const Indices2d &indices, Point &translMin_, Scalar &translRes_);
+    template void computeMaximaInd<Grid2d, Indices2d, PeakFinder2d, 2>(std::vector<Indices2d> &indicesMax, Grid2d &grid_, PeakFinder2d &peakFinder_);
     template void computeMaxima<Grid2d, Indices2d, PeakFinder2d, 2>(VectorPoint &translMax, Grid2d &grid, PeakFinder2d &peakFinder, Point &translMin_, Scalar &translRes_); // explicit instantiation for 2d version of computeMaxima()
-    // template void computeMaximaInd<Grid2d, Indices2d, PeakFinder2d, 2>(std::vector<Indices2d> &indicesMax, Grid2d &grid_, PeakFinder2d &peakFinder_);
-    // template Point getTranslation<Indices2d, 2>(const Indices2d &indices, Point &translMin_, Scalar &translRes_);
 
-    // using computeMaxima2d = ...;
+    // using computeMaxima2d = ...; //??
 
 } // end of namespace
 
